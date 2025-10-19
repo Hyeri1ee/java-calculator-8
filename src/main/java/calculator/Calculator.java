3ff72ddf120b903989d4014d91aa2d53a9a3808b
Hyeri1ee <haerizian10@gmail.com>
2025-10-19 17:02:37 +0900

    refactor(Calculator): 포괄적인 예외처리를 위해 파싱 부분 추가
    - 포괄적인 예외처리
    	1. 공백 포함 입력 차단
    	2. 구분자 위치 문제 (시작/끝/연속)
    	3. 빈 문자열 문제
    	4. 숫자가 아닌 값 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,28 +2,28 @@ package calculator;
 
 public class Calculator {
     private final InputParser parser;
-    
+
     public Calculator() {
         this.parser = new InputParser();
     }
-    
+
     public int add(String input) {
         //\n 변환 처리
         if (input.startsWith("//") && input.contains("\\n")) {
             input = input.replace("\\n", "\n");
         }
-        
+
         ParsedInput parsedInput = parser.parse(input);
         return calculateSum(parsedInput.getNumbers());
     }
-    
+
     private int calculateSum(String[] numbers) {
         int sum = 0;
-        
+
         for (String number : numbers) {
             sum += Integer.parseInt(number.trim());
         }
-        
+
         return sum;
     }
 }
